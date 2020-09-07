149986b3f3bd1912505205bb8f569a23f1ee1303
carefree-request-1153 <sgold5757@gmail.com>
2020-09-07 02:37:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,4 +27,8 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens]
 }
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
 // Add your functions and code here
