49a4906b8b91179531cbf88b4dcd15114727a80e
carefree-request-1153 <sgold5757@gmail.com>
2020-09-07 02:32:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
-function destructivelyAppendKitten(name) {
-  var kittens = ["Milo", "Otis", "Garfield"]
-  kittens.push(name)
-  return kittens
-}
-
-function destructivelyPrependKitten(name) {
-  var kittens = ["Milo", "Otis", "Garfield"]
-  kittens.upshift(name)
-  return kittens
-}
-
-function destructivelyRemoveLastKitten() {
-  kittens.pop
-  return kittens
-}
-
-function destructivelyRemoveFirstKitten() {
-  kittens.shift
-  return kittens
-}
 // Add your functions and code here
