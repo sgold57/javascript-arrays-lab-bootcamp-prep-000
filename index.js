ab9bafd6314480c8849def1e421386992ee14cf9
carefree-request-1153 <sgold5757@gmail.com>
2020-09-07 02:33:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
 // Add your functions and code here
