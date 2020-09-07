7031cd2216955ba8501c6606d9d9a53d92783896
carefree-request-1153 <sgold5757@gmail.com>
2020-09-07 02:38:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,4 +31,8 @@ function prependKitten(name) {
 function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1)
 }
+
+function removeFirstKitten() {
+  return kittens.slice(1, kittens.length)
+}
 // Add your functions and code here
